fb4983c2315e6a7e902cd401c89a312bade08b95
Tarun Gupta <tarungupta.convegenius@gmail.com>
2016-04-03 22:19:21 +0530

    Popups corrected and hamburger underline corrected and social links added in hamburger menu

--- static/js/index.js
+++ static/js/index.js
@@ -2,10 +2,110 @@ angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 
 .controller('AppCtrl', function($scope, $mdDialog, $mdMedia) {
 
-  $scope.showTabDialog = function(ev) {
+  $scope.showAmazonDialog = function(ev) {
     $mdDialog.show({
       controller: DialogController,
-      templateUrl: 'tabDialog.tmpl.html',
+      templateUrl: 'amazon.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+  };
+
+  $scope.showGoogleDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'google.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+  };
+
+  $scope.showBoostedDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'boosted.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+  };
+
+  $scope.showDjiDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'dji.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+  };
+
+  $scope.showFlipkartDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'flipkart.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+  };
+
+  $scope.showJugnooDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'jugnoo.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+  };
+
+  $scope.showLystDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'lyst.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+  };
+
+  $scope.showNikeDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'nike.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+  };
+
+  $scope.showOyoDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'oyo.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+  };
+
+  $scope.showProhuntDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'prohunt.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+  };
+
+  $scope.showTeslaDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'tesla.tmpl.html',
       parent: angular.element(document.body),
       targetEvent: ev,
       clickOutsideToClose:true
